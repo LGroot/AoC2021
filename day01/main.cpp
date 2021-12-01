8e724f5c86a288d7b515913af73c97be43a4e6bb
Laurens Groot <groot@scm.com>
2021-12-01 14:51:03 +0100

    Combine diff and count in one step with std::inner_product

--- day01/main.cpp
+++ day01/main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <numeric>
 #include <algorithm>
+#include <functional>
 
 int main() {
   std::ifstream input("input");
@@ -12,19 +13,16 @@ int main() {
     readings.push_back(std::stoi(line));
   }
 
-
-  std::vector<int> differences(readings.size());
-  std::adjacent_difference(readings.begin(), readings.end(), differences.begin()); 
-  int increasing_measurements = std::count_if(differences.begin()+1, differences.end(), [](int i){return i > 0;});
+  int increasing_measurements = std::inner_product(readings.begin()+1, readings.end(), readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
   std::cout << "Day 01 part 1: " << increasing_measurements << "\n"; 
-
 
   std::vector<int> windowed_readings(readings.size());
   std::transform(readings.begin()+1, readings.end(), readings.begin(), windowed_readings.begin(), std::plus<>{});
   std::transform(readings.begin()+2, readings.end(), windowed_readings.begin(), windowed_readings.begin(), std::plus<>{});
-  windowed_readings.erase(windowed_readings.end()-2, windowed_readings.end());
-  std::vector<int> windowed_differences(windowed_readings.size());
-  std::adjacent_difference(windowed_readings.begin(), windowed_readings.end(), windowed_differences.begin()); 
-  int increasing_windowed_measurements = std::count_if(windowed_differences.begin()+1, windowed_differences.end(), [](int i){return i > 0;});
+  int increasing_windowed_measurements = std::inner_product(windowed_readings.begin()+1, windowed_readings.end()-2, windowed_readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
+
+  //Ugly oneliner instead of generating the summed sliding window first
+  //int increasing_windowed_measurements = std::inner_product(readings.begin(), readings.end()-3, readings.begin()+1, 0, std::plus<>(), [](const int& i, const int& j){return i+(&i)[1]+(&i)[2] < j+(&j)[1]+(&j)[2];});
+
   std::cout << "Day 01 part 2: " << increasing_windowed_measurements << "\n";
 }
