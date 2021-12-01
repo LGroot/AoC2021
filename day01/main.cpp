dc3e56cb0a5bb0755e6ceebbc3f0123b1d1832d5
Laurens Groot <groot@scm.com>
2021-12-01 15:05:31 +0100

    Use smarter comparison for part 2, no summing needed.

--- day01/main.cpp
+++ day01/main.cpp
@@ -16,13 +16,8 @@ int main() {
   int increasing_measurements = std::inner_product(readings.begin()+1, readings.end(), readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
   std::cout << "Day 01 part 1: " << increasing_measurements << "\n"; 
 
-  std::vector<int> windowed_readings(readings.size());
-  std::transform(readings.begin()+1, readings.end(), readings.begin(), windowed_readings.begin(), std::plus<>{});
-  std::transform(readings.begin()+2, readings.end(), windowed_readings.begin(), windowed_readings.begin(), std::plus<>{});
-  int increasing_windowed_measurements = std::inner_product(windowed_readings.begin()+1, windowed_readings.end()-2, windowed_readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
-
-  //Ugly oneliner instead of generating the summed sliding window first
-  //int increasing_windowed_measurements = std::inner_product(readings.begin(), readings.end()-3, readings.begin()+1, 0, std::plus<>(), [](const int& i, const int& j){return i+(&i)[1]+(&i)[2] < j+(&j)[1]+(&j)[2];});
+  // We only need to compare readings[x] with readings[x+3] because the shared readings cancel out
+  int increasing_windowed_measurements = std::inner_product(readings.begin()+3, readings.end(), readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
 
   std::cout << "Day 01 part 2: " << increasing_windowed_measurements << "\n";
 }
