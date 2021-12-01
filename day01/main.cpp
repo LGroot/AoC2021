83aae1d99b2d6d53dd43b1835abc948efacdd368
Laurens Groot <groot@scm.com>
2021-12-01 15:10:07 +0100

    Use function object std::greater instead of lambda comparison.

--- day01/main.cpp
+++ day01/main.cpp
@@ -13,11 +13,11 @@ int main() {
     readings.push_back(std::stoi(line));
   }
 
-  int increasing_measurements = std::inner_product(readings.begin()+1, readings.end(), readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
+  int increasing_measurements = std::inner_product(readings.begin()+1, readings.end(), readings.begin(), 0, std::plus<>(), std::greater<>());
   std::cout << "Day 01 part 1: " << increasing_measurements << "\n"; 
 
   // We only need to compare readings[x] with readings[x+3] because the shared readings cancel out
-  int increasing_windowed_measurements = std::inner_product(readings.begin()+3, readings.end(), readings.begin(), 0, std::plus<>(), [](int i, int j){return i > j;});
+  int increasing_windowed_measurements = std::inner_product(readings.begin()+3, readings.end(), readings.begin(), 0, std::plus<>(), std::greater<>());
 
   std::cout << "Day 01 part 2: " << increasing_windowed_measurements << "\n";
 }
